bc40e0f8845e45fafd65bb588f102cd58b639cd8
Iza Dziura <izadziura@wp.pl>
2018-03-18 08:57:20 +0100

    Add innerText

--- js/script.js
+++ js/script.js
@@ -2,11 +2,5 @@ var count = document.getElementsByClassName('button');
 console.log(count.length);
 
 for (i = 0; i < count.length; i++) {
-	innerText = count[i];
-	console.log(count[i]);
+	console.log(count[i].innerText);
 }
-
-/*for (i = 0; i < count.length; i++) {
-	var array = count[i];
-	console.log(array);
-}*/
